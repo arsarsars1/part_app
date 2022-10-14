66cacdf32e931523d6d73d566e31dceae51c6847
leo-elstin <leoelstin@gmail.com>
2022-10-14 10:15:12 +0530

    version bump

--- lib/view/profile/profile.dart
+++ lib/view/profile/profile.dart
@@ -33,6 +33,8 @@ class _ProfileState extends State<Profile> {
   TextEditingController emailController = TextEditingController();
   TextEditingController academyController = TextEditingController();
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -51,204 +53,207 @@ class _ProfileState extends State<Profile> {
       appBar: AppBar(
         title: const Text('Admin Profile Details'),
       ),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: 32.h,
-          ),
-          Center(
-            child: Container(
-              width: 75.w,
-              height: 75.w,
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: NetworkImage(
-                    'https://cdn-icons-png.flaticon.com/512/552/552721.png',
+      body: Form(
+        key: formKey,
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 32.h,
+            ),
+            Center(
+              child: Container(
+                width: 75.w,
+                height: 75.w,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                      'https://cdn-icons-png.flaticon.com/512/552/552721.png',
+                    ),
+                  ),
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.primaryColor,
+                    width: 2,
                   ),
                 ),
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: AppColors.primaryColor,
-                  width: 2,
-                ),
-              ),
-              child: Stack(
-                children: [
-                  Image.network(
-                    'https://cdn-icons-png.flaticon.com/512/552/552721.png',
-                    color: Colors.white,
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      width: 24.w,
-                      height: 24.w,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black54,
-                        border: Border.all(
+                child: Stack(
+                  children: [
+                    Image.network(
+                      'https://cdn-icons-png.flaticon.com/512/552/552721.png',
+                      color: Colors.white,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        width: 24.w,
+                        height: 24.w,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black54,
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 2,
+                          ),
+                        ),
+                        child: const Icon(
+                          Icons.edit_outlined,
+                          size: 16,
                           color: Colors.white,
-                          width: 2,
                         ),
                       ),
-                      child: const Icon(
-                        Icons.edit_outlined,
-                        size: 16,
-                        color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            CommonField(
+              suffixIcon: const Icon(
+                Icons.check_circle_outline_outlined,
+                color: Colors.greenAccent,
+              ),
+              fillColor: AppColors.disabledColor,
+              disabled: true,
+              textColor: Colors.black,
+              title: 'Your Phone Number *',
+              onChange: (value) {},
+              initialValue: context.read<AuthCubit>().phoneNumber,
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            Center(
+              child: Button(
+                width: 200.w,
+                height: 30.h,
+                onTap: () {},
+                title: 'Change Phone Number',
+              ),
+            ),
+            SizedBox(
+              height: 8.h,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      'Is the above number your whatsapp number ?',
+                      style: Theme.of(context).textTheme.bodyText1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 25.0,
+                    padding: const EdgeInsets.only(right: 16),
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: CupertinoSwitch(
+                        trackColor: AppColors.grey500,
+                        value: selected,
+                        onChanged: (value) {
+                          setState(() {
+                            selected = !selected;
+                          });
+                        },
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 16.h,
-          ),
-          CommonField(
-            suffixIcon: const Icon(
-              Icons.check_circle_outline_outlined,
-              color: Colors.greenAccent,
-            ),
-            fillColor: AppColors.disabledColor,
-            disabled: true,
-            textColor: Colors.black,
-            title: 'Your Phone Number *',
-            onChange: (value) {},
-            initialValue: context.read<AuthCubit>().phoneNumber,
-          ),
-          SizedBox(
-            height: 16.h,
-          ),
-          Center(
-            child: Button(
-              width: 200.w,
-              height: 30.h,
-              onTap: () {},
-              title: 'Change Phone Number',
-            ),
-          ),
-          SizedBox(
-            height: 8.h,
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 5,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text(
-                    'Is the above number your whatsapp number ?',
-                    style: Theme.of(context).textTheme.bodyText1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  height: 25.0,
-                  padding: const EdgeInsets.only(right: 16),
-                  child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: CupertinoSwitch(
-                      trackColor: AppColors.grey500,
-                      value: selected,
-                      onChanged: (value) {
-                        setState(() {
-                          selected = !selected;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 16.h,
-          ),
-          if (!selected)
-            Column(
-              children: [
-                CommonField(
-                  title: 'Whatsapp Phone Number *',
-                  onChange: (value) {
-                    waNumber = value;
-                  },
-                  hint: 'Eg: 9876543210',
-                ),
-                const SizedBox(
-                  height: 20,
                 ),
               ],
             ),
-          CommonField(
-            controller: nameController,
-            title: 'Enter you name *',
-            hint: 'Name',
-            onChange: (value) {
-              name = value;
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CommonField(
-            controller: emailController,
-            title: 'Enter Email *',
-            hint: 'Eg: contact@polestar.com',
-            onChange: (value) {
-              email = value;
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CommonField(
-            onTap: datePicker,
-            disabled: true,
-            controller: dobController,
-            hint: 'dd/mm/yyyy',
-            title: 'Date of Birth *',
-            onChange: (value) {},
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CommonField(
-            title: 'Gender *',
-            onChange: (value) {
-              gender = value?.title;
-            },
-            hint: 'Select Gender',
-            dropDown: true,
-            dropDownItems: DefaultValues().genders,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CommonField(
-            controller: academyController,
-            title: 'Academy Name *',
-            hint: 'Enter the academy name',
-            onChange: (value) {
-              academyName = value;
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CommonField(
-            title: 'Academy Type *',
-            onChange: (value) {
-              academyType = value?.title;
-            },
-            hint: 'Select Academy Type',
-            dropDown: true,
-            dropDownItems: context.read<CountryCubit>().academyTypes,
-          ),
-        ],
+            SizedBox(
+              height: 16.h,
+            ),
+            if (!selected)
+              Column(
+                children: [
+                  CommonField(
+                    title: 'Whatsapp Phone Number *',
+                    onChange: (value) {
+                      waNumber = value;
+                    },
+                    hint: 'Eg: 9876543210',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            CommonField(
+              controller: nameController,
+              title: 'Enter you name *',
+              hint: 'Name',
+              onChange: (value) {
+                name = value;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CommonField(
+              controller: emailController,
+              title: 'Enter Email *',
+              hint: 'Eg: contact@polestar.com',
+              onChange: (value) {
+                email = value;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CommonField(
+              onTap: datePicker,
+              disabled: true,
+              controller: dobController,
+              hint: 'dd/mm/yyyy',
+              title: 'Date of Birth *',
+              onChange: (value) {},
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CommonField(
+              title: 'Gender *',
+              onChange: (value) {
+                gender = value?.title;
+              },
+              hint: 'Select Gender',
+              dropDown: true,
+              dropDownItems: DefaultValues().genders,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CommonField(
+              controller: academyController,
+              title: 'Academy Name *',
+              hint: 'Enter the academy name',
+              onChange: (value) {
+                academyName = value;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CommonField(
+              title: 'Academy Type *',
+              onChange: (value) {
+                academyType = value?.title;
+              },
+              hint: 'Select Academy Type',
+              dropDown: true,
+              dropDownItems: context.read<CountryCubit>().academyTypes,
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: SizedBox(
         height: 100.h,
@@ -257,18 +262,10 @@ class _ProfileState extends State<Profile> {
           child: Center(
             child: Button(
               onTap: () {
-                if (name == null ||
-                    email == null ||
-                    dob == null ||
-                    gender == null) {
-                  Alert(context).show(message: 'Error invalid input!');
-                  return;
-                }
                 if (!RegExp(emailRegex).hasMatch(email!)) {
                   Alert(context).show(message: 'Error enter a valid email!');
                   return;
                 }
-// todo api calls
               },
               title: 'Update',
             ),
