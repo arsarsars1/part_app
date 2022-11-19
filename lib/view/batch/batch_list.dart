147dd782a4de818d23a12dd9a0fe5089cf0c18b2
leo-elstin <leoelstin@gmail.com>
2022-11-19 19:07:06 +0530

    add batch UI & API

--- lib/view/batch/batch_list.dart
+++ lib/view/batch/batch_list.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:part_app/view/batch/add_batch.dart';
+import 'package:part_app/view/components/branch_field.dart';
 import 'package:part_app/view/components/common_bar.dart';
 import 'package:part_app/view/components/components.dart';
+import 'package:part_app/view/components/tab_button.dart';
+import 'package:part_app/view/constants/constant.dart';
+import 'package:part_app/view_model/cubits.dart';
 
 class BatchesPage extends StatefulWidget {
   static const route = '/batches';
+
   const BatchesPage({Key? key}) : super(key: key);
 
   @override
@@ -15,6 +20,8 @@ class BatchesPage extends StatefulWidget {
 class _BatchesPageState extends State<BatchesPage> {
   @override
   Widget build(BuildContext context) {
+    var cubit = context.read<BatchCubit>();
+
     return Scaffold(
       appBar: const CommonBar(title: 'Batches'),
       body: Column(
@@ -36,6 +43,102 @@ class _BatchesPageState extends State<BatchesPage> {
               ),
             ),
           ),
+          BranchField(
+            onSelect: (value) {},
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CommonField(
+            title: 'Search',
+            hint: 'Search Branch',
+            onChange: (value) {},
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TabButton(
+              onChange: (String value) {},
+              options: const [
+                'Ongoing Batches',
+                'Completed Batches',
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.liteDark,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Batch 1',
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    Text(
+                      'Active Students: 10',
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            color: AppColors.primaryColor,
+                          ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Active Students: 10',
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        color: AppColors.primaryColor,
+                      ),
+                ),
+                Text(
+                  'Course Subject',
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Text(
+                  'Trainer - Leo, Yami',
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        color: AppColors.primaryColor,
+                      ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Mon',
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Text(
+                  'Tue',
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Text(
+                  'Mon',
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
