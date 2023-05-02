dd3981a90672a369ca026d7a5def2d756f8bd973
akhilpg <akhil.pg@lifesignals.com>
2023-05-03 03:34:01 +0530

    Need to check academy_type_id is same as admins academy_type_id

--- lib/view/home/components/home_banner.dart
+++ lib/view/home/components/home_banner.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:part_app/model/extensions.dart';
 import 'package:part_app/view/components/components.dart';
 import 'package:part_app/view/constants/app_colors.dart';
 import 'package:part_app/view_model/cubits.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeBanner extends StatefulWidget {
   const HomeBanner({Key? key}) : super(key: key);
@@ -13,26 +16,54 @@ class HomeBanner extends StatefulWidget {
 
 class _HomeBannerState extends State<HomeBanner> {
   PageController controller = PageController();
-  double? currentPage = 0;
+  int currentPage = 0;
+  late Timer timer;
+  bool isButtonVisible = false;
+  var tempBanner;
+  var bannersToDisplay = [];
+  bool _initialized = false;
+  late DateTime fromTime = DateTime.now();
+  late DateTime toTime = DateTime.now();
+  late String formattedString;
 
   @override
   void initState() {
     super.initState();
-    controller.addListener(() {
+    /*controller.addListener(() {
       if (controller.page != currentPage) {
         setState(() {
-          currentPage = controller.page;
         });
       }
-    });
+    });*/
+
+    // startTimer();
   }
 
   @override
   Widget build(BuildContext context) {
     var cubit = context.read<HomeCubit>();
+    /*if(cubit.banner != null && cubit.banner!.isNotEmpty && !_initialized){
+      for (int i = 0; i < cubit.banner!.length; i++) {
+        if(cubit.banner![i].isActive == 1){
+          bannersToDisplay.add(cubit.banner![i]);
+        }
+      }
+    }*/
+    // _initialized = true;
     return BlocBuilder<HomeCubit, HomeState>(
       builder: (context, state) {
-        return cubit.banner != null && cubit.banner!.isNotEmpty
+
+        if(cubit.banner != null && cubit.banner!.isNotEmpty && !_initialized){
+          for (int i = 0; i < cubit.banner!.length; i++) {
+            if(cubit.banner![i].isActive == 1){
+              bannersToDisplay.add(cubit.banner![i]);
+            }
+          }
+          _initialized = true;
+          startTimer();
+        }
+        // return cubit.banner != null && cubit.banner!.isNotEmpty
+        return bannersToDisplay.isNotEmpty
             ? Column(
                 children: [
                   Padding(
@@ -42,7 +73,8 @@ class _HomeBannerState extends State<HomeBanner> {
                       child: ListView.builder(
                         shrinkWrap: true,
                         scrollDirection: Axis.horizontal,
-                        itemCount: cubit.banner?.length ?? 0,
+                        // itemCount: cubit.banner?.length ?? 0,
+                        itemCount: bannersToDisplay.length ?? 0,
                         itemBuilder: (context, index) {
                           bool selected = index == currentPage;
                           return Container(
@@ -66,9 +98,12 @@ class _HomeBannerState extends State<HomeBanner> {
                     height: 141.h,
                     child: PageView.builder(
                       controller: controller,
-                      itemCount: cubit.banner?.length ?? 0,
+                      // itemCount: cubit.banner?.length ?? 0,
+                      itemCount: bannersToDisplay.length ?? 0,
                       itemBuilder: (context, index) {
-                        var banner = cubit.banner?[index];
+                        // var banner = cubit.banner?[index];
+                        var banner = bannersToDisplay[index];
+                        tempBanner = banner;
                         return Container(
                           decoration: BoxDecoration(
                             image: DecorationImage(
@@ -103,9 +138,12 @@ class _HomeBannerState extends State<HomeBanner> {
                                           banner.description ?? '',
                                         ),
                                         Text(
-                                          '${banner.startTime?.toTime()} '
-                                          'To ${banner.endTime?.toTime()}, '
-                                          '${banner.endTime?.formattedString()}',
+                                          // '${banner.startTime?.toTime()} '
+                                          '${fromTime.toTime()} '
+                                          // 'To ${banner.endTime?.toTime()}, '
+                                          'To ${toTime.toTime()}, '
+                                          // '${banner.endTime?.formattedString()}',
+                                          '${formattedString}',
                                           style: Theme.of(context)
                                               .textTheme
                                               .bodyText1
@@ -116,13 +154,24 @@ class _HomeBannerState extends State<HomeBanner> {
                                       ],
                                     ),
                                   ),
-                                  Button(
+                                  if (isButtonVisible)
+                                    Button(
                                     backgroundColor: AppColors.defaultBlue,
                                     height: 22.h,
                                     width: 97.w,
                                     fontSize: 10.sp,
-                                    onTap: () {},
-                                    title: 'Register Now',
+                                    onTap: () async {
+                                      var url = banner.extUrl!.isEmpty?
+                                      'https://partapp.in/' : banner.extUrl!;
+                                      final uri = Uri.parse(url);
+                                      if (await canLaunchUrl(uri)) {
+                                        await launchUrl(uri);
+                                      } else {
+                                        throw 'Could not launch $url';
+                                      }
+                                    },
+                                    title: banner.extUrlButtonText!.isEmpty?
+                                    'Register Now' : banner.extUrlButtonText!,
                                   ),
                                 ],
                               ),
@@ -137,5 +186,24 @@ class _HomeBannerState extends State<HomeBanner> {
             : const Offstage();
       },
     );
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (currentPage < 10) {
+        currentPage++;
+        setState(() {
+          isButtonVisible = tempBanner!.extUrl!.isEmpty?
+          false : true;
+          fromTime = tempBanner!.startTime;
+          toTime = tempBanner!.endTime;
+          formattedString = toTime.formattedString();
+        });
+      } else {
+        currentPage = 0;
+      }
+      controller.animateToPage(currentPage as int,
+          duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+    });
   }
 }
