646a78dbdf38a213cc9e8131614bbe99ebb37844
Akhil A Ambady <akhilappu.ambady@gmail.com>
2023-05-06 02:03:25 +0530

    Updated the UI for trainer list page

--- lib/view/trainer/trainer_view.dart
+++ lib/view/trainer/trainer_view.dart
@@ -25,7 +25,6 @@ class _TrainerPageState extends State<TrainerPage> {
 
     // get the trainers list
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      context.read<TrainerCubit>().getActiveInactiveTrainers(active: true);
       context.read<BranchCubit>().getBranches();
     });
   }
@@ -39,132 +38,155 @@ class _TrainerPageState extends State<TrainerPage> {
         title: 'Trainers List',
       ),
       body: Column(
-        // physics: const NeverScrollableScrollPhysics(),
         children: [
-          Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 16.w,
-                right: 16.w,
-                top: 16.h,
-              ),
-              child: Button(
-                height: 30.h,
-                onTap: () {
-                  cubit.fromBranch = false;
-                  cubit.selectedBranches = [];
-                  Navigator.pushNamed(context, AddTrainer.route);
-                },
-                title: 'Add New Trainer',
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20.h,
-          ),
-          BlocBuilder<BranchCubit, BranchState>(
-            builder: (context, state) {
-              return CommonField(
-                title: 'Branch',
-                hint: 'Select Branch',
-                dropDown: true,
-                dropDownItems: branchCubit.dropDownBranches(),
-                onChange: (value) {
-                  branchId = value.id;
-                  cubit.searchTrainers(branchId, query: null);
-                },
-              );
-            },
-          ),
-          SizedBox(
-            height: 20.h,
-          ),
-          CommonField(
-            prefixIcon: const Icon(Icons.search),
-            title: 'Search',
-            maxLines: 1,
-            hint: 'Search By Trainer Name Or Phone Number',
-            dropDown: false,
-            textInputAction: TextInputAction.search,
-            onSubmit: (value) {
-              if (value.toString().isNotEmpty) {
-                query = value;
-                cubit.searchTrainers(branchId, query: value);
-              } else {
-                cubit.getTrainers();
-                query = null;
-              }
-            },
-            onChange: (value) {
-              if (value.toString().isEmpty) {
-                query = null;
-                cubit.getTrainers();
-              }
-            },
-          ),
-          SizedBox(
-            height: 20.h,
-          ),
-          TabButton(
-            onChange: (String value) {
-              temp = value;
-              if (value == "Active Trainers") {
-                context
-                    .read<TrainerCubit>()
-                    .getActiveInactiveTrainers(active: true);
-              } else {
-                context
-                    .read<TrainerCubit>()
-                    .getActiveInactiveTrainers(clean: true);
-              }
-
-              // cubit.filterTrainers(active: value == 'Active Trainers');
-            },
-            options: const [
-              'Active Trainers',
-              'Inactive Trainers',
-            ],
-          ),
-          BlocBuilder<TrainerCubit, TrainerState>(
-            builder: (context, state) {
-              if (state is FetchingTrainers) {
-                return const Padding(
-                  padding: EdgeInsets.all(64.0),
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              }
-
-              // ignore: prefer_is_empty
-              if (cubit.trainers?.length == 0) {
-                return Padding(
-                  padding: const EdgeInsets.all(64.0),
-                  child: Center(
-                    child: Text(
-                      query == null
-                          ? 'Empty List'
-                          : 'Sorry, No Matching Results Found.',
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: 16.w,
+                      right: 16.w,
+                      top: 16.h,
+                    ),
+                    child: Button(
+                      height: 30.h,
+                      onTap: () {
+                        cubit.fromBranch = false;
+                        cubit.selectedBranches = [];
+                        Navigator.pushNamed(context, AddTrainer.route);
+                      },
+                      title: 'Add New Trainer',
                     ),
                   ),
-                );
-              }
-              return Expanded(
-                child: TrainerList(
-                  trainers: cubit.trainers ?? [],
-                  onSelect: (Trainer trainer) async {
-                    context.read<TrainerCubit>().getTrainerDetails(
-                          trainerId: trainer.id,
-                        );
-                    await Navigator.pushNamed(context, TrainerDetails.route);
-                    // ignore: use_build_context_synchronously
-                    context.read<TrainerCubit>().getActiveInactiveTrainers(
-                        active: temp == "Active Trainers" ? true : false);
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                BlocBuilder<BranchCubit, BranchState>(
+                  builder: (context, state) {
+                    return CommonField(
+                      title: 'Branch',
+                      hint: 'Select Branch',
+                      dropDown: true,
+                      dropDownItems: branchCubit.dropDownBranches(),
+                      onChange: (value) {
+                        branchId = value.id;
+                        setState(() {
+                          cubit.searchTrainers(branchId, query: null);
+                        });
+                      },
+                    );
                   },
                 ),
-              );
-            },
+                SizedBox(
+                  height: 20.h,
+                ),
+                CommonField(
+                  prefixIcon: const Icon(Icons.search),
+                  title: 'Search',
+                  maxLines: 1,
+                  hint: 'Search By Trainer Name Or Phone Number',
+                  dropDown: false,
+                  textInputAction: TextInputAction.search,
+                  onSubmit: (value) {
+                    if (value.toString().isNotEmpty) {
+                      query = value;
+                      cubit.searchTrainers(branchId, query: value);
+                    } else {
+                      cubit.getTrainers();
+                      query = null;
+                    }
+                  },
+                  onChange: (value) {
+                    if (value.toString().isEmpty) {
+                      query = null;
+                      cubit.getTrainers();
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                branchId != null
+                    ? Column(
+                        children: [
+                          TabButton(
+                            onChange: (String value) {
+                              temp = value;
+                              if (value == "Active Trainers") {
+                                cubit.getActiveInactiveTrainers(
+                                    branchId: branchId, active: true);
+                              } else {
+                                cubit.getActiveInactiveTrainers(
+                                    branchId: branchId, clean: true);
+                              }
+                            },
+                            options: const [
+                              'Active Trainers',
+                              'Inactive Trainers',
+                            ],
+                          ),
+                          BlocBuilder<TrainerCubit, TrainerState>(
+                            builder: (context, state) {
+                              if (state is FetchingTrainers) {
+                                return const Padding(
+                                  padding: EdgeInsets.all(64.0),
+                                  child: Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                );
+                              }
+
+                              // ignore: prefer_is_empty
+                              if (cubit.trainers?.length == 0) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(64.0),
+                                  child: Center(
+                                    child: Text(
+                                      query == null
+                                          ? 'Empty List'
+                                          : 'Sorry, No Matching Results Found.',
+                                    ),
+                                  ),
+                                );
+                              }
+                              return TrainerList(
+                                trainers: cubit.trainers ?? [],
+                                onSelect: (Trainer trainer) async {
+                                  context
+                                      .read<TrainerCubit>()
+                                      .getTrainerDetails(
+                                        trainerId: trainer.id,
+                                      );
+                                  await Navigator.pushNamed(
+                                      context, TrainerDetails.route);
+                                  // ignore: use_build_context_synchronously
+                                  context
+                                      .read<TrainerCubit>()
+                                      .getActiveInactiveTrainers(
+                                          branchId: branchId,
+                                          active: temp == "Active Trainers"
+                                              ? true
+                                              : false);
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      )
+                    : const Padding(
+                        padding: EdgeInsets.all(64.0),
+                        child: Center(
+                          child: Text(
+                            'Select a branch',
+                          ),
+                        ),
+                      ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/view_model/trainer/trainer_cubit.dart
+++ lib/view_model/trainer/trainer_cubit.dart
@@ -96,14 +96,23 @@ class TrainerCubit extends Cubit<TrainerState> {
 
     if (response?.trainers != null) {
       _trainers = response?.trainers?.data ?? [];
-      filterTrainers(active: _isActive);
+      // filterTrainers(active: _isActive);
     } else {
       emit(FailedToFetchTrainers('Failed to fetch the trainers'));
     }
   }
 
+  /// reset
+  void reset() {
+    _trainers?.clear();
+    emit(TrainerInitial());
+  }
+
   Future getActiveInactiveTrainers(
-      {bool nextPage = false, active = false, clean = false}) async {
+      {int? branchId,
+      bool nextPage = false,
+      active = false,
+      clean = false}) async {
     String? arg = "";
     emit(FetchingTrainers());
     TrainerResponse? response = active
@@ -134,30 +143,30 @@ class TrainerCubit extends Cubit<TrainerState> {
             _trainers = [...?_trainers, element];
           }
         }
-        filterTrainers(active: _isActive);
+        // filterTrainers(active: _isActive);
       }
     }
   }
 
-  List<Trainer>? filterTrainers({required bool active}) {
-    _isActive = active;
-    log("=======================================================================");
-    _trainers?.forEach((element) {
-      log(element.toJson().toString());
-    });
-    log("=======================================================================");
-    List<Trainer>? list = _trainers?.where((element) {
-      if (active) {
-        return element.trainerDetail?[0].isActive == 1;
-      } else {
-        return element.trainerDetail?[0].isActive == 0;
-      }
-    }).toList();
+  // List<Trainer>? filterTrainers({required bool active}) {
+  //   _isActive = active;
+  //   log("=======================================================================");
+  //   _trainers?.forEach((element) {
+  //     log(element.toJson().toString());
+  //   });
+  //   log("=======================================================================");
+  //   List<Trainer>? list = _trainers?.where((element) {
+  //     if (active) {
+  //       return element.trainerDetail?[0].isActive == 1;
+  //     } else {
+  //       return element.trainerDetail?[0].isActive == 0;
+  //     }
+  //   }).toList();
 
-    filteredTrainers = list ?? [];
+  //   filteredTrainers = list ?? [];
 
-    return list;
-  }
+  //   return list;
+  // }
 
   Future searchTrainers(int? branchID, {String? query}) async {
     _trainers = [];
@@ -178,7 +187,7 @@ class TrainerCubit extends Cubit<TrainerState> {
     }
 
     if (_trainers != null) {
-      filterTrainers(active: _isActive);
+      // filterTrainers(active: _isActive);
       emit(SearchedTrainers());
     } else {
       emit(FailedToFetchTrainers('Failed to fetch the trainers'));
@@ -341,4 +350,6 @@ class TrainerCubit extends Cubit<TrainerState> {
     // clear profile pic
     await CachedNetworkImage.evictFromCache(profileUrl);
   }
+
+  void clear() {}
 }
