028a42c2130ede29b59affa306d76662afe9231f
AKHIL V S <akhilv.reubro@gmail.com>
2023-09-09 15:02:23 +0530

    overflow issue fix - welcome screen

--- lib/view/components/common_field.dart
+++ lib/view/components/common_field.dart
@@ -115,18 +115,15 @@ class CommonField extends StatelessWidget {
                         : dropDownItems?.map((e) {
                             return DropdownMenuItem(
                               value: e,
-                              child: SizedBox(
-                                width: 300,
-                                child: Text(
-                                  e.title ?? '',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyLarge
-                                      ?.copyWith(
-                                        color: Colors.white,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                ),
+                              child: Text(
+                                e.title ?? '',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge
+                                    ?.copyWith(
+                                      color: Colors.white,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
                               ),
                             );
                           }).toList(),
